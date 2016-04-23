fc80d95bfd916c9dd95b16fa14eaf36c64648a08
liuyatao <739697033@qq.com>
2016-04-23 21:48:56 +0800

    开发环境搭建

---
+++ chifan.sql
@@ -0,0 +1,60 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : admin
+Source Server Version : 50626
+Source Host           : localhost:3306
+Source Database       : chifan
+
+Target Server Type    : MYSQL
+Target Server Version : 50626
+File Encoding         : 65001
+
+Date: 2016-04-23 21:48:21
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for charge
+-- ----------------------------
+DROP TABLE IF EXISTS `charge`;
+CREATE TABLE `charge` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of charge
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for order
+-- ----------------------------
+DROP TABLE IF EXISTS `order`;
+CREATE TABLE `order` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT COMMENT '0 充值 1 消费',
+  `Date` date DEFAULT NULL,
+  `Number` float(11,0) DEFAULT NULL,
+  `UserName` varchar(255) DEFAULT NULL,
+  `Type` int(255) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of order
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `UserName` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
